1f1b8d1a5ac836143cde9eb1c6696e294ab83f1f
alirezakkt <alirea.kh.kkt@gmail.com>
2023-02-10 01:05:41 +0330

    Ui handeled

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    weatherApiBaseUrl : 'ng g environments',
+    XRapidAPIHostHeaderName : 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue : 'dark-sky.p.rapidapi.com',
+    XRapidAPIKeyHeaderName : 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderNValue:
+    'dfed53d4b2mshcf9ade6039ce931p1130b1jsn6b8ac4d243a9'
+
+};
